cc8a14e7832da8289282ecbeaf295889d08a0679
dohaelee <dev.dohaelee@gmail.com>
2023-02-23 12:26:38 +0900

    Import Project

--- src/common/covers.js
+++
@@ -1,9 +0,0 @@
-import cover001 from './covers/001.jpg';
-import cover002 from './covers/002.jpg';
-import cover003 from './covers/003.jpg';
-import cover004 from './covers/004.jpg';
-import cover005 from './covers/005.jpg';
-
-const covers = [null, cover001, cover002, cover003, cover004, cover005];
-
-export default covers;
